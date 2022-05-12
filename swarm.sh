d91147cf3e2c410f5a6eaf454a0416d2834e9480
mja00 <teamnirvana2@gmail.com>
2022-05-12 11:33:53 -0400

    Script update

--- swarm.sh
+++ swarm.sh
@@ -12,35 +12,35 @@ fi
 # If the first arg is build or deploy, we want to build the docker image
 if [ "$first_arg" = "build" ] || [ "$first_arg" = "deploy" ]; then
   echo "Building the docker image"
-  docker-compose -f docker-compose.prod.yml build
+  sudo docker-compose -f docker-compose.prod.yml build
   exit 0
 fi
 
 # If the first arg is up, we want to start the docker containers
 if [ "$first_arg" = "up" ]; then
   echo "Starting the docker containers"
-  docker-compose -f docker-compose.prod.yml up -d
+  sudo docker-compose -f docker-compose.prod.yml up -d
   exit 0
 fi
 
 # If the first arg is down, we want to stop the docker containers
 if [ "$first_arg" = "down" ]; then
   echo "Stopping the docker containers"
-  docker-compose -f docker-compose.prod.yml down
+  sudo docker-compose -f docker-compose.prod.yml down
   exit 0
 fi
 
 # If the first arg is restart, we want to restart the docker containers
 if [ "$first_arg" = "restart" ]; then
   echo "Restarting the docker containers"
-  docker-compose -f docker-compose.prod.yml restart
+  sudo docker-compose -f docker-compose.prod.yml restart
   exit 0
 fi
 
 # If the first arg is migrate, we want to run the migrations
 if [ "$first_arg" = "migrate" ]; then
   echo "Running the migrations"
-  docker-compose -f docker-compose.prod.yml exec web flask db upgrade
+  sudo docker-compose -f docker-compose.prod.yml exec web flask db upgrade
   exit 0
 fi
 
@@ -50,11 +50,11 @@ if [ "$first_arg" = "full_update" ]; then
   echo "Updating the repo"
   git pull
   echo "Building the docker image"
-  docker-compose -f docker-compose.prod.yml build
+  sudo docker-compose -f docker-compose.prod.yml build
   echo "Starting the docker containers"
-  docker-compose -f docker-compose.prod.yml up -d
+  sudo docker-compose -f docker-compose.prod.yml up -d
   echo "Running the migrations"
-  docker-compose -f docker-compose.prod.yml exec web flask db upgrade
+  sudo docker-compose -f docker-compose.prod.yml exec web flask db upgrade
   exit 0
 fi
 
@@ -64,8 +64,8 @@ if [ "$first_arg" = "upgrade" ]; then
   echo "Updating the repo"
   git pull
   echo "Building the docker image"
-  docker-compose -f docker-compose.prod.yml build
+  sudo docker-compose -f docker-compose.prod.yml build
   echo "Starting the docker containers"
-  docker-compose -f docker-compose.prod.yml up -d
+  sudo docker-compose -f docker-compose.prod.yml up -d
   exit 0
 fi
