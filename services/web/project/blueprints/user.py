1feff718fa813f61d13f3a3bb39ec626ccb53f77
mja00 <teamnirvana2@gmail.com>
2022-05-27 12:24:54 -0400

    Handling users that haven't linked MC

--- services/web/project/blueprints/user.py
+++ services/web/project/blueprints/user.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from datetime import timedelta
 
 import humanize
-from flask import Blueprint, render_template, redirect, url_for
+import uuid as uuid_lib
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import current_user
 
 from ..decorators import minecraft_authenticated
@@ -12,9 +13,30 @@ from ..extensions import cache
 user_bp = Blueprint('user', __name__)
 
 
+# The order of searching here is:
+# 1. The user's Minecraft UUID
+# 2. The user's Minecraft username
+# 3. The user's portal username
 @user_bp.route('/<uuid>')
 def get_user(uuid):
-    user = User.query.filter_by(minecraft_uuid=uuid).first()
+    if uuid == "null":
+        flash('This user has not linked a Minecraft account.', 'warning')
+        return redirect(url_for('index'))
+
+    # Check if the uuid is a valid UUID
+    try:
+        uuid_lib.UUID(uuid)
+        user = User.query.filter_by(minecraft_uuid=uuid).first()
+    except ValueError:
+        # Wasn't a valid UUID check by minecraft username
+        user = User.query.filter_by(minecraft_username=uuid).first()
+        if user is None:
+            # Not a valid minecraft username, check by portal username
+            user = User.query.filter_by(username=uuid).first()
+
+    if user is None:
+        flash('User not found. It must be a valid UUID, Minecraft username, or portal username.', 'danger')
+        return redirect(url_for('index'))
     # Get their most recent application
     application = Application.query.filter_by(user_id=user.id).order_by(Application.id.desc()).first()
     if application:
